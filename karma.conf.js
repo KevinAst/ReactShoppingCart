a21216ba205c4370d717a5845fe3ad3f07902ea2
Kevin Bridges <kevin@wiiBridges.com>
2016-01-27 15:19:34 -0600

    don't process emacs backup files (ex: foo.js~)

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'src/**/*.kmocha.js*'
+      'src/**/*.kmocha.js',
+      'src/**/*.kmocha.jsx'
     ],
 
 
